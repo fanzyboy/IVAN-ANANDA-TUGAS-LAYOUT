13d4601afeb1e4c240d9532037565e907e452bdb
fanzyboy <ivanananda12345@gmail.com>
2024-09-09 19:35:06 +0700

    first commit

---
+++ app/src/main/java/com/ivan/pagerelativelayout/tablelayout.kt
@@ -0,0 +1,20 @@
+package com.ivan.pagerelativelayout
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class tablelayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tablelayout)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
